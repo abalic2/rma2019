813e1e5a8ccff964c90136c9b0499673adcf2cba
abalic2 <abalic2@etf.unsa.ba>
2019-04-11 23:55:33 +0200

    layout fragmenata

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -71,6 +71,16 @@ public class KvizoviAkt extends AppCompatActivity {
             }
         });
 
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                pozicijaKliknutog = position;
+                Intent myIntent = new Intent(KvizoviAkt.this, IgrajKvizAkt.class);
+                myIntent.putExtra("kviz", odabraniKvizovi.get(position));
+                KvizoviAkt.this.startActivityForResult(myIntent,5);
+            }
+        });
+
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
